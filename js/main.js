9378e52f361f91b18e26b6060bee5ea6643fb097
Nela Petria <nela_petria@yahoo.com>
2018-07-21 12:16:22 +0300

    feat: aria-label implemented

--- js/main.js
+++ js/main.js
@@ -160,7 +160,7 @@ createRestaurantHTML = restaurant => {
   image.alt = `Exterior photo of the restaurant ${restaurant.name}`;
   li.append(image);
 
-  const name = document.createElement("h2");
+  const name = document.createElement("h3");
   name.innerHTML = restaurant.name;
   li.append(name);
 
